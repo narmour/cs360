d9fb0f2b4f1ccb2168582e2b443f018dc77f5331
nick armour <n.armour0@gmail.com>
2016-11-18 19:25:11 -0800

    it works now, can read in from file

--- lab7/Board.java
+++ lab7/Board.java
@@ -1,13 +1,19 @@
 import java.util.*;
+import java.io.*;
 class Board{
 
     //boolean array to represent game board
     private boolean[] brd;
+
+    //solution move set
+    private ArrayList<Move> solution; 
     
     //constructor
     public Board(){
         //init 2d array
         brd = new boolean[49];
+
+        solution= new ArrayList<Move>();
 
         //set board
         brd = setBoard();
@@ -17,34 +23,26 @@ class Board{
     //copy constructor
     public Board(Board b){
         brd = new boolean[49];
-        brd = b.brd;
+        solution= new ArrayList<Move>();
+        for(int i=0;i<49;i++)
+            brd[i] = b.brd[i];
     }
 
     boolean[] setBoard(){
         boolean[] _brd = new boolean[49];
-        /*
-        // hard code start for now
-        
-        for(int i =0;i < 49;i++){
-                if(i==0 || i==1 || i==5 || i==6 || i==7
-                        ||i==8 || i==12 || i==13|| i==24
-                        || i ==35 || i==36 || i==40 || i==41
-                        || i ==42 || i==43 || i==47 || i==48)
 
-                    _brd[i] = false;
-                else
-                    _brd[i]= true;
-            }
-          */  
-        
-        for(int i =0;i < 49;i++)
-            _brd[i] = false;
-        
-        _brd[9] = true;
-        _brd[16] = true;
-        _brd[3] = true;
-        //_brd[24] = true;
-       
+        // read in from game.txt
+        String line = new String();
+        try{
+            Scanner s = new Scanner(new File("game.txt"));
+            line = s.nextLine();
+        }catch(FileNotFoundException e){
+            System.out.println("no game.txt");
+            return _brd;
+        }
+        String[] n = line.split(" ");
+        for(String x:n)
+            _brd[Integer.parseInt(x)] = true;
 
         return _brd;
     }
@@ -53,6 +51,8 @@ class Board{
         for(int i =0;i <49;i++){
                 if(brd[i])
                     System.out.print("x   ");
+                else if (isPlayable(i))
+                    System.out.print("_   ");
                 else
                     System.out.print("    ");
                 if((i+1)%7==0)
@@ -75,14 +75,14 @@ class Board{
         Board t =  new Board(b);
 
         //flip the first and last
-        b.brd[m.m[0]] = true;
-        b.brd[m.m[1]] = false;
-        b.brd[m.m[2]] = false;
+        t.brd[m.m[0]] = true;
+        t.brd[m.m[1]] = false;
+        t.brd[m.m[2]] = false;
         return t;
     }
 
 
-
+    // returns a list of all possible moves on board x
     ArrayList<Move>nextMoves(Board x){
         ArrayList<Move> moveList = new ArrayList<Move>();
         for(int i =0;i<49;i++){
@@ -90,25 +90,25 @@ class Board{
             if(isPlayable(i) && x.brd[i] == false){
                 //check 2 left
                 if( i%7 -2 >=0 && x.brd[i-1] &&x.brd[i-2]){
-                    System.out.println("(" + (i) + " " + (i-1) + " " +(i-2) +")");
+               //     System.out.println("(" + (i) + " " + (i-1) + " " +(i-2) +")");
                     Move m = new Move(i,i-1,i-2);
                     moveList.add(m);
                 }
                 //check 2 right
                 if( i%7+2<7 && x.brd[i+1] &&x.brd[i+2]){
-                    System.out.println("(" + i + " " + (i+1) + " " +( i+2) +")");
+             //       System.out.println("(" + i + " " + (i+1) + " " +( i+2) +")");
                     Move m = new Move(i,i+1,i+2);
                     moveList.add(m);
                 }
                 //check 2 up
                 if(i-14 >=0 && x.brd[i-7] && x.brd[i-14]){
-                    System.out.println("(" + i + " " + (i-7) + " " +( i-14) +")");
+           //         System.out.println("(" + i + " " + (i-7) + " " +( i-14) +")");
                     Move m = new Move(i,i-7,i-14);
                     moveList.add(m);
                 }
                 //check 2 down
                 if(i <35 && x.brd[i+7] && x.brd[i+14]){
-                    System.out.println("(" + i + " " + (i+7) + " " +( i+14) +")");
+         //           System.out.println("(" + i + " " + (i+7) + " " +( i+14) +")");
                     Move m = new Move(i,i+7,i+14);
                     moveList.add(m);
                 }
@@ -134,7 +134,9 @@ class Board{
         for(Move m:next){
            Board y = move(m,x);
             if(solve(y,mseq)){
-                y.printBoard();
+                x.printBoard();
+                //m.printMove();
+                solution.add(m);
                 mseq = push(m,mseq);
                 return true;
             }
@@ -156,13 +158,20 @@ class Board{
         return true;
     }
 
+    //prints a list of moves
+    void printMoves(ArrayList<Move> m){
+        for(Move x : m)
+            x.printMove();
+    }
+
 
     public static void main(String[] a){
         Board b = new Board();
-        b.printBoard();
         ArrayList<Move> moves = new ArrayList<Move>();
-        //b.solve(b,moves);
-       b.nextMoves(b);
+//        b.printBoard();
+        b.solve(b,moves);
+        b.printMoves(b.solution);
+       //b.nextMoves(b);
 
 
 
